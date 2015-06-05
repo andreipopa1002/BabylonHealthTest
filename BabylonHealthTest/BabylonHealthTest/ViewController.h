1d53e9dd15aa6802276736e362b215e9edd7b65e
Popa Andrei <andrei.popa.1002@gmail.com>
2015-06-05 13:09:16 +0100

    Initial commit
    Added AFNetworking
    Added ignore file

---
+++ BabylonHealthTest/BabylonHealthTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BabylonHealthTest
+//
+//  Created by Popa Andrei on 05/06/2015.
+//  Copyright (c) 2015 Popa Andrei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BabylonHealthTest/Pods/Target Support Files/Pods-BabylonHealthTest-AFNetworking/Pods-BabylonHealthTest-AFNetworking-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BabylonHealthTest_AFNetworking : NSObject
+@end
+@implementation PodsDummy_Pods_BabylonHealthTest_AFNetworking
+@end
